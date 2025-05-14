7fc565925630c7511165305a667a8a72f7b073c8
siddhapurahet <84630752+siddhapurahet@users.noreply.github.com>
2025-05-14 12:52:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+// server.js -> This is responsible for starting the server side of the application. The framework used is Express.
 const express = require('express');
 const path = require('path');
 
